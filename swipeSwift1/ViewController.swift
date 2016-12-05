48aa29ad2812f2c1e28ee8f9311602da87870663
ftwdjw <ftwdjw@gmail.com>
2016-12-04 19:28:22 -0600

    initial commit

--- swipeSwift1/ViewController.swift
+++ swipeSwift1/ViewController.swift
@@ -9,16 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var testView: UIView!
+    
+    
+    
+     //initialize left and right UI swipr gesture recognizers
+     let leftSwipe = UISwipeGestureRecognizer()
+     let rightSwipe = UISwipeGestureRecognizer()
+    
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        testView.addGestureRecognizer(leftSwipe)
+        leftSwipe.direction = .left
+        
+         testView.addGestureRecognizer(rightSwipe)
+        rightSwipe.direction = .right
+        
+        testView.isUserInteractionEnabled  = true
+        
+        leftSwipe.addTarget(self, action: #selector(ViewController.swipedViewLeft))
+        rightSwipe.addTarget(self, action: #selector(ViewController.swipedViewRight))
+        
+        print("view did load")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func swipedViewLeft(){
+        
+                print("Swipe Left")
+                let tapAlert = UIAlertController(title: "Swiped", message: "You just swiped left", preferredStyle: UIAlertControllerStyle.alert)
+                tapAlert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
+                self.present(tapAlert, animated: true, completion: nil)
+               
+               
+            }
+            
+          func swipedViewRight(){
+                print("Swipe Right")
+                let tapAlert = UIAlertController(title: "Swiped", message: "You just swiped right", preferredStyle: UIAlertControllerStyle.alert)
+                tapAlert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
+                self.present(tapAlert, animated: true, completion: nil)
+           
+            }
+        
+        
+ 
+    
 
 
 }
